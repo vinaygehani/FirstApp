68a8f4e1a95e0af61ae50f0b4942b28ba5d11f16
vinaygehani <icehammer@gmail.com>
2016-04-27 18:25:34 +0530

    New fns

--- app/src/main/java/com/example/android/firstapp/MainActivity.java
+++ app/src/main/java/com/example/android/firstapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.android.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(2);
+        displayPrice(2);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.qty_txt);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_txt);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
