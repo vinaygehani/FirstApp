1fa352ad1d037644309938558212f749796430fc
Vinay <icehammer@gmail.com>
2016-04-27 22:52:06 +0530

    a

--- app/src/main/java/com/example/android/firstapp/MainActivity.java
+++ app/src/main/java/com/example/android/firstapp/MainActivity.java
@@ -8,6 +8,11 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    int quantity=1;
+
+    String priceString = "Total Price";
+    String qtyString="Quantity";
+    String thankString="Thank You!! Order placed for ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +25,35 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2);
+        TextView thanksTextView = (TextView) findViewById(R.id.thanks);
+        thanksTextView.setText(thankString + NumberFormat.getCurrencyInstance().format(quantity*5));
     }
 
+    public void increaseQuantity(View view) {
+        quantity++;
+        display(quantity);
+
+    }
+
+    public void decreaseQuantity(View view) {
+        if(quantity>1)
+            quantity--;
+        display(quantity);
+
+    }
     /**
      * This method displays the given quantity value on the screen.
      */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(
-                R.id.qty_txt);
+                R.id.qty_value);
         quantityTextView.setText("" + number);
+
+        displayPrice(number*5);
     }
 
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_txt);
+        TextView priceTextView = (TextView) findViewById(R.id.price_value);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
